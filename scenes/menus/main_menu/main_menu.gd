273064ab9e6d61f454e81ea6dd00f1a4bafe815d
Jason <j989li@uwaterloo.ca>
2024-10-05 16:36:45 -0400

    re-organized code a bit

--- scenes/menus/main_menu/main_menu.gd
+++ scenes/menus/main_menu/main_menu.gd
@@ -12,10 +12,10 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/game_menu/game.tscn")
 
 func _on_help_button_down() -> void:
-	get_tree().change_scene_to_file("res://scenes/help_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/help_menu/help_menu.tscn")
 
 func _on_exit_pressed() -> void:
 	print("Exit Pressed")
