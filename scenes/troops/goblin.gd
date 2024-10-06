35a52c2b6f8f2b7a86c159c2fe9f5749aae0c6bb
RehmanMujtaba <rehmanmujtaba@outlook.com>
2024-10-06 07:38:44 -0400

    Sprites collied with tower

--- scenes/troops/goblin.gd
+++ scenes/troops/goblin.gd
@@ -2,7 +2,14 @@ extends CharacterBody2D
 
 const SPEED = 200
 const direction = 1 # Moving right
+var isFriendly = true
+
+func setisFriendly(status: bool) -> void:
+	isFriendly = status 
 
 func _physics_process(delta: float) -> void:
-	velocity.x = direction * SPEED
+	if isFriendly and position.x >= 975:
+		velocity.x = 0
+	else:
+		velocity.x = direction * SPEED		
 	move_and_slide()
